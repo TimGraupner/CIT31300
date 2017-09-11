f44ef6a5e1e7925a7d655b2d6f23b958747e818c
TimGraupner <timjgraupner@gmail.com>
2017-09-10 22:23:16 -0400

    Coconuts
    PHP debugging assignment

---
+++ a1/CIT313_Fall2017-master/Debug-Exercise-master/webconfig.php
@@ -0,0 +1,5 @@
+<?php
+
+define('ABSOLUTE_PATH', '/home/tgraupne/htdocs/a1/CIT313_Fall2017-master/Debug-Exercise-master');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:21881/a1/CIT313_Fall2017-master/Debug-Exercise-master');
+?>
