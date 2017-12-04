36e4fd5cc1272fdd398c95138c690ba653b56525
TimGraupner <timjgraupner@gmail.com>
2017-12-03 23:01:46 -0500

    Final
    The final, part 1

---
+++ final/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// Display errors in production mode
+
+// let's get started
+require 'application/router.php';
---
+++ final/views/home/index.php
@@ -0,0 +1,15 @@
+<?php
+include('views/elements/header.php');?>
+<div class="container">
+	<div class="page-header">
+		<h1>Welcome to my blog!</h1>
+		<p>This is my blog! It's run using an MVC. Bacon ipsum dolor amet tri-tip pork chop pork belly, andouille flank rump capicola ham burgdoggen ham hock fatback. Flank pastrami short ribs ribeye. Boudin filet mignon brisket beef salami ball tip. Flank pork belly swine fatback kevin. Sausage prosciutto bresaola, alcatra corned beef brisket tongue landjaeger short loin tenderloin meatball pork belly pork chop.</p>
+
+		<p>Brisket sirloin ham corned beef. Beef ham sirloin jerky bacon hamburger sausage ribeye pork biltong t-bone beef ribs jowl bresaola tri-tip. Alcatra buffalo swine shoulder fatback pork belly tail sirloin brisket chicken drumstick pork loin kielbasa. Burgdoggen pork belly ham biltong tri-tip cupim strip steak, bacon tenderloin tongue chicken frankfurter bresaola jowl ribeye.</p>
+    <h1>Latest News from <?php echo $title;?></h1>
+  </div>
+    <?php
+    echo $data;
+    ?>
+</div>
+<?php include('views/elements/footer.php');?>
